52e35a44f034328f97ea946921a261d5b201e172
issjagut <issjagut@fc5ca088-f436-11dd-9d1a-b59b2e1864b6>
2009-02-11 12:01:07 +0000

    - adapted device_002-config to my personal hardware-layout - modified compile.bat to compile device_001 and device_002

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -4,11 +4,11 @@ make clean
 make 
 cp device_001.hex ../fw_bootloader
 cp device_001.eep ../fw_bootloader
-rem cd ..
-rem cd device_002
-rem make clean
-rem make
-rem cp device_002.hex ../fw_bootloader
-rem cp device_002.eep ../fw_bootloader
+cd ..
+cd device_002
+make clean
+make
+cp device_002.hex ../fw_bootloader
+cp device_002.eep ../fw_bootloader
 
 @pause
